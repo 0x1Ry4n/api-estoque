178390eca54ced659ceb0fdc897e988ca15a385b
Ryan <ryanlucas2018gz@gmail.com>
2024-10-07 17:18:25 -0300

    ...

--- crud/src/main/java/com/apiestoque/crud/CrudApplication.java
+++ crud/src/main/java/com/apiestoque/crud/CrudApplication.java
@@ -2,10 +2,8 @@ package com.apiestoque.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class CrudApplication {
 
 	public static void main(String[] args) {
